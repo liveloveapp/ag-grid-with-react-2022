24ead1aec54855ee57b961b77ad68a49090df089
Mike Ryan <mike.ryan52@gmail.com>
2022-10-10 14:56:25 -0700

    coldef-03-challenge

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -9,18 +9,10 @@ interface RowData {
 }
 
 export default function Grid() {
-  /**
-   * 1. Specify the `columnDef` column definition state and bind to the `<AgGridReact>` component.
-   */
   const [columnDefs] = useState<ColDef<RowData>[]>([
     { headerName: "Name", field: "name" },
     { headerName: "Color", field: "color" },
   ]);
-
-  /**
-   * 2. Set the `rowData` state using the existing `data` import (data.products),
-   *    and bind to the `<AgGridReact>` component.
-   */
   const [rowData] = useState<RowData[]>(data.products);
 
   return (
