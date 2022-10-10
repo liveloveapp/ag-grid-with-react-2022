992fa97d38d062d3fa33af4d15193e2ec0c30d9d
Mike Ryan <mike.ryan52@gmail.com>
2022-10-10 14:26:43 -0700

    coldef-01-solution

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -12,15 +12,20 @@ export default function Grid() {
   /**
    * 1. Specify the `columnDef` column definition state and bind to the `<AgGridReact>` component.
    */
+  const [columnDefs] = useState<ColDef<RowData>[]>([
+    { headerName: "Name", field: "name" },
+    { headerName: "Color", field: "color" },
+  ]);
 
   /**
    * 2. Set the `rowData` state using the existing `data` import (data.products),
    *    and bind to the `<AgGridReact>` component.
    */
+  const [rowData] = useState<RowData[]>(data.products);
 
   return (
     <div className="ag-theme-alpine">
-      <AgGridReact></AgGridReact>
+      <AgGridReact columnDefs={columnDefs} rowData={rowData}></AgGridReact>
     </div>
   );
 }
