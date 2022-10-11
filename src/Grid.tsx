1d8dd55e7486d0824def39ed2def84be91b9bfeb
Mike Ryan <mike.ryan52@gmail.com>
2022-10-10 21:23:46 -0700

    cellrendering-04-challenge

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -1,27 +1,82 @@
-import React, { useState } from "react";
-import { ColDef } from "ag-grid-community";
+import React, { useMemo, useState } from "react";
+import { ColDef, ICellRendererParams } from "ag-grid-community";
 import { AgGridReact } from "ag-grid-react";
 import data from "./data.json";
 
 interface RowData {
-  name: string;
-  color: string;
+  id: number;
+  accountId: number;
+  customerId: number;
+  dateOfOrder: Date;
+  account: {
+    id: number;
+    customerId: number;
+    accountNumber: string;
+    name: string;
+    amount: string;
+  };
+  customer: {
+    name: string;
+  };
+  orderItems: {
+    orderId: number;
+    productId: number;
+    quantity: number;
+  }[];
+  total: number;
 }
 
 export default function Grid() {
-  /**
-   * 1. Specify the `columnDef` column definition state and bind to the `<AgGridReact>` component.
-   */
   const [columnDefs] = useState<ColDef<RowData>[]>([
-    { headerName: "Name", field: "name" },
-    { headerName: "Color", field: "color" },
+    {
+      headerName: "Customer Name",
+      field: "customer.name",
+      filter: true,
+    },
+    {
+      headerName: "Account No",
+      field: "account.accountNumber",
+      filter: "agNumberColumnFilter",
+    },
+    {
+      headerName: "Date of Order",
+      field: "dateOfOrder",
+      filter: "agDateColumnFilter",
+    },
+    {
+      headerName: "Total",
+      field: "total",
+      filter: "agNumberColumnFilter",
+      cellRenderer: (params: ICellRendererParams) => {
+        if (params.value === undefined) {
+          return null;
+        }
+        return new Intl.NumberFormat("en-US", {
+          style: "currency",
+          currency: "USD",
+        }).format(params.value);
+      },
+    },
   ]);
-
-  /**
-   * 2. Set the `rowData` state using the existing `data` import (data.products),
-   *    and bind to the `<AgGridReact>` component.
-   */
-  const [rowData] = useState<RowData[]>(data.products);
+  const rowData = useMemo<RowData[]>(() => {
+    return data.orders.map((order) => ({
+      ...order,
+      // create Date object value for dateOfOrder field
+      dateOfOrder: new Date(`${order.dateOfOrder.slice(0, 10)}T00:00:00.0`),
+      account: data.accounts.find((account) => account.id === order.accountId)!,
+      customer: data.customers.find(
+        (customer) => customer.id === order.customerId
+      )!,
+      orderItems: data.orderItems.filter((item) => item.orderId === order.id),
+      total: data.orderItems
+        .filter((item) => item.orderId === order.id)
+        .map(
+          (item) =>
+            data.products.find((product) => product.id === item.productId)!
+        )
+        .reduce((prev, current) => prev + Number(current.price), 0),
+    }));
+  }, [data]);
 
   return (
     <div className="ag-theme-alpine">
